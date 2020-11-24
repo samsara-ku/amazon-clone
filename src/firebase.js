b67937c80e3fc84657365df7255ed1cbb0bd2e86
yunhoe <yunhoe@deepnatural.ai>
2020-11-24 20:46:01 +0900

    Project repository settings are finished

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBuTfFrztZit5RPJOmfPW2hRBEp2_j6VSA',
+  authDomain: 'clone-62ed0.firebaseapp.com',
+  databaseURL: 'https://clone-62ed0.firebaseio.com',
+  projectId: 'clone-62ed0',
+  storageBucket: 'clone-62ed0.appspot.com',
+  messagingSenderId: '666245472780',
+  appId: '1:666245472780:web:8ed85742907be798edc429',
+  measurementId: 'G-H9LJ0Y6BW6',
+}
